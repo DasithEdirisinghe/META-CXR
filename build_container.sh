0f09d63d3c10dfb0a1e82de4ec7334f98a05ef43
Dasith Edirisinghe <udasith@gmail.com>
2025-07-10 19:19:47 +0530

    build container script update with dockerhub name

--- build_container.sh
+++ build_container.sh
@@ -3,9 +3,9 @@
 IMAGE_NAME="meta-cxr"
 IMAGE_TAG="1.0.0"
 IMAGE_FULL="${IMAGE_NAME}:${IMAGE_TAG}"
-DOCKERHUB_IMAGE="meta-cxr-1.0.0"  # Replace with exact repo/image name on Docker Hub if needed
+DOCKERHUB_IMAGE="dasithdev/${IMAGE_FULL}"
 
-# Check if image exists locally
+# Check if local image exists
 if [[ "$(docker images -q ${IMAGE_FULL} 2> /dev/null)" == "" ]]; then
     echo "Image ${IMAGE_FULL} not found locally. Pulling from Docker Hub..."
     docker pull ${DOCKERHUB_IMAGE}
@@ -14,4 +14,5 @@ else
     echo "Image ${IMAGE_FULL} already exists locally."
 fi
 
+# Build new Docker image on top
 docker build -t meta-cxr:2.0.0 -f Dockerfile .
